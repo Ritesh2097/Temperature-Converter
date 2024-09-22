6218acdfdc92438a1f6043b8099a93327a440091
Ritesh2097 <144782253+Ritesh2097@users.noreply.github.com>
2024-09-22 08:28:38 +0530

    Added a naming convention

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function converter() {
-    let temp = document.getElementById("temp").value;
-    let temptype = document.getElementById("temptype").value;
-    if (temptype == "celsius") {
-      document.getElementById("converted_value").innerHTML = temp * (9 / 5) + 32;    
-    } else if(temptype == "fahrenheit"){
-      document.getElementById("converted_value").innerHTML = (temp - 32) * (5 / 9);    
+    let temperatureInput = document.getElementById("temperatureInput").value;
+    let temperaturetype = document.getElementById("temperaturetype").value;
+    if (temperaturetype == "celsius") {
+      document.getElementById("convertedvalue").innerHTML = temperatureInput * (9 / 5) + 32;    
+    } else if(temperaturetype == "fahrenheit"){
+      document.getElementById("convertedvalue").innerHTML = (temperatureInput - 32) * (5 / 9);    
     }
     else{
-      document.getElementById("converted_value").innerHTML = Invalid;
+      document.getElementById("convertedvalue").innerHTML = Invalid;
     }
   
     return false;
